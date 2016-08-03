cbafa2c8f2902b24ea3d78e9d7a5819311a7cfb2
Stéphan Gaud <stephgoeseast@gmail.com>
2016-08-03 14:50:45 +0100

    change the each method for a while loop

---
+++ directory-ex4.rb
@@ -0,0 +1,44 @@
+#Introduction
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+
+  #Creating students array
+  students = []
+
+  #Getting input from user
+  name = gets.chomp
+
+  #While the name is NOT empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  #Return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villain Academy"
+  puts "--------------"
+end
+
+def print(names)
+  x = 0
+  while x < names.length
+    puts "#{x + 1}. #{names[x][:name]}, #{names[x][:cohort]} cohort"
+    x = x + 1
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+students = input_students
+print_header
+print(students)
+print_footer(students)
