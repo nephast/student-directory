0952c4f1871a6619dcd2fcdfdd97c92b569d5a11
Stéphan Gaud <stephgoeseast@gmail.com>
2016-08-04 23:05:35 +0100

    break interactive menu method in smaller methods

--- directory.rb
+++ directory.rb
@@ -1,50 +1,50 @@
+@students = [] # an empty array accessible to all methods
+
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts ""
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
 def input_students
-  puts ""
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-
-
-  #Creating students array
-  students = []
-
   #Getting input from user
   name = gets.chomp
-
   #While the name is NOT empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     #get another name from the user
     name = gets.chomp
   end
-  #Return the array of students
-  students
 end
 
 def print_header
@@ -53,15 +53,15 @@ def print_header
   puts "--------------"
 end
 
-def print(names)
-  names.each_with_index do |student, index|
+def print_students_list
+  @students.each_with_index do |student, index|
   puts "#{index+1}. #{student[:name]}, (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
+def print_footer
   puts ""
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
